3de0d42b59563e28411ff5c95d2221a3905bcd99
NevenaM12 <nevena.mijailovic000@gmail.com>
2024-08-22 20:24:03 +0200

    Upgraded cppcheck

--- cppcheck/run_cppcheck.sh
+++ cppcheck/run_cppcheck.sh
@@ -2,4 +2,6 @@
 
 SOURCE_DIR="../2048"
 
-cppcheck --enable=all --inconclusive --suppress=missingIncludeSystem $SOURCE_DIR
+cppcheck --enable=all --inconclusive --suppress=missingInclude --output-file=output_improved.xml --xml ../2048_improved
+
+cppcheck-htmlreport --file=output_improved.xml --report-dir=report_improved
